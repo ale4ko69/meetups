5f9d7d02af500c24a820ed6b8946b5d493a9e95a
Alexey kagansky <info@corp.narod.co.il>
2018-03-01 14:36:24 +0200

    Create initfb.js

---
+++ src/firebase/initfb.js
@@ -0,0 +1,12 @@
+const initfb = (function() {
+	return {
+		apiKey: '',
+		authDomain: '',
+		databaseURL: '',
+		projectId: '',
+		storageBucket: '',
+		messagingSenderId: ''
+	};
+})();
+
+export default initfb;
